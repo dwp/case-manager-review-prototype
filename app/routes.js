933a6a8aae92099b428e6eb14a5a3158f3016e00
hannah.noble <hannah.noble@engineering.digital.dwp.gov.uk>
2024-05-30 23:13:10 +0100

    comms record iteration 1

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,34 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+//COMMUNICATION RECORD V.1
+// What direction of contact are you adding?
+router.post('/comms-record/start-record', function(request, response) {
+    var claimingSelf = request.session.data['contact-direction']
+    if (claimingSelf == 'inbound'){
+        response.redirect('/comms-record/')
+    } else if (claimingSelf == "outbound") {
+        response.redirect('/comms-record/outbound/contact-reasons-1')
+    }
+})
+
+// Why did we contact them?
+router.post('/comms-record/outbound/contact-reasons-1', function(request, response) {
+    var claimingSelf = request.session.data['who']
+    if (claimingSelf == 'post'){
+        response.redirect('/comms-record/outbound/contact-sub-reasons')
+    } else if (claimingSelf == "change-of-circs") {
+        response.redirect('/comms-record/outbound/inbound/contact-sub-reasons')
+    }
+})
+
+// Why did we contact them?
+router.post('/comms-record/outbound/contact-sub-reasons', function(request, response) {
+        response.redirect('/comms-record/security-questions')
+})
+
+// Security questions
+router.post('/comms-record/security-questions', function(request, response) {
+    response.redirect('/comms-record/addition-complete')
+})
