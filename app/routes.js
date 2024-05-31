449576fe0b8f54c53389b58c819ca48ada9ad2f6
hannah.noble <hannah.noble@engineering.digital.dwp.gov.uk>
2024-05-31 11:18:42 +0100

    iteration 1 for comms record

--- app/routes.js
+++ app/routes.js
@@ -13,19 +13,20 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.post('/comms-record/start-record', function(request, response) {
     var claimingSelf = request.session.data['contact-direction']
     if (claimingSelf == 'inbound'){
-        response.redirect('/comms-record/')
+        response.redirect('/comms-record/inbound/contact-reasons-1')
     } else if (claimingSelf == "outbound") {
         response.redirect('/comms-record/outbound/contact-reasons-1')
     }
 })
 
+//OUTBOUND
 // Why did we contact them?
 router.post('/comms-record/outbound/contact-reasons-1', function(request, response) {
     var claimingSelf = request.session.data['who']
     if (claimingSelf == 'post'){
         response.redirect('/comms-record/outbound/contact-sub-reasons')
     } else if (claimingSelf == "change-of-circs") {
-        response.redirect('/comms-record/outbound/inbound/contact-sub-reasons')
+        response.redirect('/comms-record/outbound/contact-sub-reasons')
     }
 })
 
@@ -37,4 +38,21 @@ router.post('/comms-record/outbound/contact-sub-reasons', function(request, resp
 // Security questions
 router.post('/comms-record/security-questions', function(request, response) {
     response.redirect('/comms-record/addition-complete')
+})
+
+
+//INBOUND
+// Why did we contact them?
+router.post('/comms-record/inbound/contact-reasons-1', function(request, response) {
+    var whyTwo = request.session.data['who']
+    if (whyTwo == 'post'){
+        response.redirect('/comms-record/inbound/contact-sub-reasons')
+    } else if (whyTwo == "change-of-circs") {
+        response.redirect('/comms-record/inbound/contact-sub-reasons')
+    }
+})
+
+ // Why did we contact them?
+router.post('/comms-record/outbound/contact-sub-reasons', function(request, response) {
+    response.redirect('/comms-record/security-questions')
 })
