bdd253038f0f7e64f72e98905a559f0db5ea2cb6
kevinsinclair <kevin.sinclair@engineering.digital.dwp.gov.uk>
2025-11-05 14:58:45 +0000

    update validate paths v2
    GitOrigin-RevId: 5b62cb834dad63960e68d3501f3deceb6ad1a5b3

--- app/views/pip-register-v3/validatePath.js
+++ app/views/pip-register-v3/validatePath.js
@@ -1,33 +1,33 @@
 const path = require('path')
 
-const allowedPaths = ['5-2-today']
+module.exports = function (res,redirectPath) {
+  const allowedPaths = ['5-2-today']
+  const validatePath = (res, redirectPath) => {
+    console.log("INSIDE VALIDATEPATH");
+    JSON.stringify(allowedPaths)
+    const basePath = getBasePath(redirectPath);
+    console.log("THE BASE PATH ",basePath)
 
-const validatePath = (res, redirectPath) => {
-  console.log("INSIDE VALIDATEPATH");
-  JSON.stringify(allowedPaths)
-  const basePath = getBasePath(redirectPath);
-  console.log("THE BASE PATH ",basePath)
+    if (allowedPaths.includes(basePath)) {
+      console.log("IN ALLOWED PATHS");
+      return res.redirect(redirectPath);
+    } else {
+      console.log("NOT IN ALLOWED PATHS");
+      const error = new Error('Client Error - Path not found on allowed path list');
+      error.clientError = 404;
+      throw error;
+    }
+  };
 
-  if (allowedPaths.includes(basePath)) {
-    console.log("IN ALLOWED PATHS");
-    return res.redirect(redirectPath);
-  } else {
-    console.log("NOT IN ALLOWED PATHS");
-    const error = new Error('Client Error - Path not found on allowed path list');
-    error.clientError = 404;
-    throw error;
-  }
-};
+  const getBasePath = (redirectPath) => {
+    console.log("INSIDE GETBASEPATH");
+    if (redirectPath.indexOf('?') > -1) {
+      console.log("DOES HAVE QUESTION MARK");
+      return redirectPath.split(path.sep).pop().split('?')[0];
+    }
+    console.log("ORIGINAL REDIRECT PATH ",redirectPath)
+    return redirectPath.split(path.sep).pop()
+  };
 
-const getBasePath = (redirectPath) => {
-  console.log("INSIDE GETBASEPATH");
-  if (redirectPath.indexOf('?') > -1) {
-    console.log("DOES HAVE QUESTION MARK");
-    return redirectPath.split(path.sep).pop().split('?')[0];
-  }
-  console.log("ORIGINAL REDIRECT PATH ",redirectPath)
-  return redirectPath.split(path.sep).pop()
-};
 
-export default validatePath;
- 
+}
